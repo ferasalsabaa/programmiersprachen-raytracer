cc8653e1ff4316567f4e606d49a85e81723d780c
Feras al sabaa <feras.alsabaa@uni-weimar.de>
2018-06-13 14:46:20 +0200

    aufgabe 1

---
+++ framework/shape.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_RUNNER
+#include "framework/shape.hpp"
+#include<iostream>
+
+// float Shape::area() const
+//{
+  //  return 0;
+//}
---
+++ framework/shape.hpp
@@ -0,0 +1,12 @@
+#ifndef Shape_HPP
+#define Shape_HPP
+class Shape
+{
+  virtual float area() const=0;
+  virtual float volume() const=0;
+};
+
+
+
+
+#endif
