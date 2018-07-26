987ce8fea1bee00e463590b935f7970c57867aba
Theresa Tobollik <theresa.tobollik@gmail.com>
2018-07-26 17:29:32 +0200

    add light container and ambient

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -1,20 +1,25 @@
 #ifndef SCENE_HPP
 #define SCENE_HPP
-#include"material.hpp"
-#include"shape.hpp"
-#include<memory>
-#include<iostream>
-#include<fstream>
-#include<sstream>
-#include<vector>
-#include<map>
-#include<set>
+#include "material.hpp"
+#include "shape.hpp"
+#include "light.hpp"
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <set>
 
-struct Scene
-{  std::string name_;
+
+struct Scene{ 
+    
+   std::string name_;
    std::map<std::string,std::shared_ptr<Material>> material_map;
    std::shared_ptr<Shape> shared_shape();
-   
+   std::vector<Light> lights;
+   Light ambient;
+
 };
 Scene open_sdf (std::string const& sdf_name);
 std::shared_ptr<Material> find_map(std::string const& name,std::map<std::string,std::shared_ptr<Material>> const& material_map);
