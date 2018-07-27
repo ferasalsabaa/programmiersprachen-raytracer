ef8a62fde3c367c072fd2441e8beea994e7b3d32
Theresa Tobollik <theresa.tobollik@gmail.com>
2018-07-27 13:02:16 +0200

    add camera class

---
+++ framework/camera.hpp
@@ -0,0 +1,21 @@
+#ifndef camera_HPP
+#define camera_HPP
+#include <iostream>
+
+class Camera {
+
+    public:
+    std::string name_;
+    float aperture_;
+
+    Camera() : name_{""}, aperture_{45} {}
+    Camera(std::string name, float aperture) : name_{name}, aperture_{aperture} {}
+
+    private:
+    //
+
+};
+
+
+
+#endif
