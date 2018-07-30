5fefa1fc21e86589cc981ac0d8ee88a4168b5889
Theresa Tobollik <theresa.tobollik@gmail.com>
2018-07-30 16:16:42 +0200

    code-transfer

---
+++ framework/code_snippet.cpp
@@ -0,0 +1,9 @@
+  Ray Renderer::shoot_ray(float x, float y){
+		glm::vec3 right= glm::cross(cam_.direction_, cam_.up_); 
+		glm::vec3 up= glm::cross(right,cam_.direction_); 
+		float fovBereich=tan(cam_.aperture_*(M_PI*0.5f/180))*2;
+		glm::vec3 r=right*( (x-0.5f)*fovBereich );
+		glm::vec3 u=up*( (y-0.5f)*fovBereich );
+		glm::vec3 tmp=cam_.direction_+r+u;
+		return Ray{cam_.origin_,glm::normalize(tmp)};
+	}
